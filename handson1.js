48709bfce35762ccc8c308ce6e5fdba912c21992
Novanda Seimahuira <38125643+Dandavanda@users.noreply.github.com>
2021-07-09 17:01:38 +0700

    [handson2] Promise

--- handson1.js
+++ handson1.js
@@ -28,8 +28,26 @@ function table(){
     document.getElementById("contentTable").innerHTML = tab;
 }
 
+const wait = time => new Promise(resolve => setTimeout(resolve, time));
+
 document.getElementById("ageBtn").addEventListener("click", function() {
     const totalAge = students.reduce((acc, students)=> acc + students.age, 0);
-    document.getElementById("avgA").innerHTML = "Average age students : " + totalAge / students.length;
 
+    async function asyncAwait() {
+        document.getElementById("step1").innerHTML = "Calculation Start" ;
+
+        await wait(2000);
+        document.getElementById("step2").innerHTML = "Second call Started";
+
+        await wait();
+        document.getElementById("step3").innerHTML = "third call started" ;
+
+        await wait(3000);
+        document.getElementById("avgA").innerHTML = "Average :" + totalAge / students.length;
+
+        document.getElementById("finish").innerHTML = "third call finished" ;
+        
+    }
+
+    asyncAwait();
   });
